f6da7614e8fcb361b27c5bf4e357b937974faf9b
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-04-27 02:38:28 +0100

    updating documentation

--- __init__.py
+++ __init__.py
@@ -3,53 +3,55 @@ from .models import db, User, Contact, shutdown_session
 from flask_jwt_extended import JWTManager
 import jwt
 from datetime import timedelta
+from dotenv import load_dotenv
+import os
 
 
 def create_app():
+    """Create a Flask app instance and initialize its configurations, database, JWT authentication,
+    blueprints, and error handlers.
+    
+    Returns:
+        Flask: A Flask app instance.
+    """
+    
+    # Initializing the Flask app from Flask class
     app = Flask(__name__)
 
-    
+    # Load the environment variables from the .env file
+    load_dotenv()
 
-    app.config["JWT_SECRET_KEY"]= "jwt_secret_key"
+    # Set the JWT secret key
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY ")
 
-    app.config["SECRET_KEY"]= "secret_key"
-    app.config["SQLALCHEMY_DATABASE_URI"]= "mysql+pymysql://user:passwd@host_ip/db"
+    # Set the Flask secret key
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
-    app.config["JWT_ACCESS_TOKEN_EXPIRES"] =timedelta(minutes=60)
-    app.config["JWT_REFRESH_TOKEN_EXPIRES"] =timedelta(minutes=60)
+    # Set the SQLAlchemy database URI
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
 
+    # Set the access token and refresh token expiration time
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=60)
+    app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(minutes=60)
 
+    # Initialize the database and create all tables
     db.init_app(app)
     with app.app_context():
         db.create_all()
 
+    # Create an instance of JWTManager and pass our app as the parameter
     jwt = JWTManager(app)
-    from jwt.exceptions import ExpiredSignatureError
 
-    @app.errorhandler(ExpiredSignatureError)
-    def handle_expired_signature_error(e):
-        response = {'message': 'Token has expired'}
-        return jsonify(response), 401
-    
-    # @jwt.expired_token_loader
-    # def expired_token_callback():
-    #     return jsonify({
-    #     'status': 401,
-    #     'message': 'The token has expired'
-    # }), 401
-
-
-
-
+    # Import and register the user_api blueprint
     from .apis.user_api import user_api
     app.register_blueprint(user_api)
 
+    # Import and register the contact_api blueprint
     from .apis.contact_api import contact_api
     app.register_blueprint(contact_api)
 
+    # Initialize the db.session.remove method to be called after each db.session.commit
     app.teardown_request(shutdown_session)
 
-
+    # Return the Flask app instance
     return app
-
-
--- models.py
+++ models.py
@@ -1,29 +1,31 @@
 from flask_sqlalchemy import SQLAlchemy
-import datetime
+from datetime import datetime
 
 
-
+""" creating an instance of SQLAlchemy  """
 db = SQLAlchemy()
 
+
 class User(db.Model):
+    """ schema for user table using 1 to many relationship """
+
     __tablename__ = "user"
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     full_name = db.Column(db.String(256), nullable=False)
     username = db.Column(db.String(256), nullable=False, unique=True)
     email = db.Column(db.String(256), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
-    created_by = db.Column(db.DateTime(), default=datetime.datetime.utcnow)
+    created_by = db.Column(db.DateTime(), default=datetime.utcnow)
     location = db.Column(db.String(64))
     about_me = db.Column(db.Text())
-    member_since = db.Column(db.DateTime(), default=datetime.datetime.utcnow)
-    contact = db.relationship("Contact", backref="user", lazy="dynamic")
-
-
-
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    contacts = db.relationship("Contact", backref="user", lazy="dynamic")
 
 
 
 class Contact(db.Model):
+    """ schema for contact table using many to 1 relationship """
+
     __tablename__ = "contact"
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     name = db.Column(db.String(256), nullable=False)
@@ -33,8 +35,11 @@ class Contact(db.Model):
     city = db.Column(db.String(256))
     country = db.Column(db.String(256))
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+
     
 def shutdown_session(exception=None):
+    """ A function to close the database at the end of each db.session.commit """
     db.session.remove()
 
 
